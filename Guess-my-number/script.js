df2ccdcce13abf1fe972673c1cb9beea98c8981e
redmirx <redmirx9@gmail.com>
2022-09-25 01:32:57 +0500

    Project-1

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -1,4 +1,15 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-console.log((document.querySelector('.message').innerHTML = 'Hello from DOM'));
-console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message').textContent);
+// console.log((document.querySelector('.message').innerHTML = 'Hello from DOM'));
+// console.log(document.querySelector('.message'));
+
+// document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 12;
+
+// document.querySelector('.guess').value = 20;
+
+let btnCheck = document.querySelector('.check');
+
+btnCheck.addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
