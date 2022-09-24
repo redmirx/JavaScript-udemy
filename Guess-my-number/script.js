7b7a1a95723a222429ce8cbba214bb7325e7ec70
redmirx <redmirx9@gmail.com>
2022-09-24 19:34:17 +0500

    Project-1

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -1,1 +1,4 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+console.log((document.querySelector('.message').innerHTML = 'Hello from DOM'));
+console.log(document.querySelector('.message'));
