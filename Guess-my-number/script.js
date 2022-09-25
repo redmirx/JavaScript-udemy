19d817fd4d7f4c006891061af3ecea68739a386d
redmirx <redmirx9@gmail.com>
2022-09-25 15:51:53 +0500

    Project1

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
+let score = 20;
 let btnCheck = document.querySelector('.check');
+
+document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.score').innerHTML = score;
 
 btnCheck.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,8 +14,22 @@ btnCheck.addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
