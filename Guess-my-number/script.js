8f6980e946008230143c8fbe04429501d8572b8f
redmirx <redmirx9@gmail.com>
2022-09-25 18:18:17 +0500

    Project-1

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 // document.querySelector('.number').textContent = secretNumber;
 // document.querySelector('.score').innerHTML = score;
@@ -18,6 +19,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   // When the guess is too high
   else if (guess > secretNumber) {
