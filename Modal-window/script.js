4c9f214fb0f5a19c37fdd24f2e76ffa541a6c56c
redmirx <redmirx9@gmail.com>
2022-09-25 22:48:58 +0500

    Project-2

--- Modal-window/script.js
+++ Modal-window/script.js
@@ -4,16 +4,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-// console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
