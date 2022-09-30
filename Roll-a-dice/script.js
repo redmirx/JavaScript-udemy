54406a305d95e96a38ba195c0ccd0852325580f5
redmirx <redmirx9@gmail.com>
2022-09-30 12:55:07 +0500

    Project-3

--- Roll-a-dice/script.js
+++ Roll-a-dice/script.js
@@ -1,10 +1,44 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-const dice = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-dice.classList.add('hidden');
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  // Generating a random dive roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // console.log(dice);
+  //Check for rolled 1: if true, swith to next player.
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
