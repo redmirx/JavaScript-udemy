21c4160245a553b20926ce8b0f4fc8804c9d35bc
redmirx <redmirx9@gmail.com>
2022-10-03 16:08:22 +0500

    section-8

--- Behind-the-scenes/script.js
+++ Behind-the-scenes/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  function printAge() {
+    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  // return age;
+}
+
+const firstName = 'Abdurakhmon';
+calcAge(2003);
