f3791f25e225af3ce692fd705b31b2b0a930586c
redmirx <redmirx9@gmail.com>
2022-11-16 16:36:15 +0500

    coding challange 4

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -177,3 +177,56 @@ const gameTime = 90;
 //     } ${time}: ${event}`
 //   );
 // }
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const tick = '✅';
+  // const convertion = text.split('_')[1].slice(0, 1).toUpperCase();
+  const arr = text.split('\n');
+  for (const [index, item] of arr.entries()) {
+    const trimmed = item.trim();
+    const toLowCaps = trimmed.toLowerCase();
+    const splitted = toLowCaps.split('_');
+    const firstWord = splitted[0];
+    const secondWord =
+      splitted[1].slice(0, 1).toUpperCase() + splitted[1].slice(1);
+    const convertion = firstWord + secondWord;
+    console.log(convertion.padEnd(20, ' ') + tick.repeat(index + 1));
+  }
+  // console.log(arr);
+});
